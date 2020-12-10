acdd37330c8ddf038f6595481af1b2d2d0a6c6eb
dnevaykin <40721547+dnevaykin@users.noreply.github.com>
2020-12-10 23:48:34 +0300

    урок - 1, задание - 3, попытка - 1

---
+++ contact.py
@@ -0,0 +1,8 @@
+
+class Contact:
+
+    def __init__(self, first_name, midle_name, last_name, nikname):
+        self.first_name = first_name
+        self.midle_name = midle_name
+        self.last_name = last_name
+        self.nikname = nikname
