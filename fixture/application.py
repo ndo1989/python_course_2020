c337b0e183fdfc048412ed395e7145c59fa48d35
dnevaykin <40721547+dnevaykin@users.noreply.github.com>
2020-12-16 22:44:44 +0300

    Задание 5 попытка 2. Реализована двухуровневая архитектура тестов

---
+++ fixture/application.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from pythoncourse2020.fixture.session import SessionHelper
+from pythoncourse2020.fixture.group import GroupHelper
+from pythoncourse2020.fixture.contact import ContactHelper
+
+class Application:
+
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
+
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/")
+
+    def destroy(self):
+        self.wd.quit()
+
