33803b6c8cfdbcbd5da168d6d91e8ac2a48bbc3c
dnevaykin <40721547+dnevaykin@users.noreply.github.com>
2020-12-20 15:28:51 +0300

    Добавил кейс на редактирования контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -37,4 +37,23 @@ class ContactHelper:
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to.alert.accept()
 
-
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        # init edit contact
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        # edit contact firm
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.midle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nikname)
+        # submit contact update
+        wd.find_element_by_name("update").click()
+        self.return_to_home_page()
