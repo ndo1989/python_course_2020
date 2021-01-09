2c8f1b09ec56b7f1e6f79c6a1bfec2c6f8be8ede
dnevaykin <40721547+dnevaykin@users.noreply.github.com>
2021-01-09 22:34:40 +0300

    добавил стравнение контактов до начала теста и после

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,5 +3,8 @@ from pythoncourse2020.model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="edwded", header="dcsdw", footer="edwedw"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -4,4 +4,7 @@ from pythoncourse2020.model.group import Group
 def test_delete_first_group(app):
     if app.group.count_group() ==0:
         app.group.create(Group(name="edwded100", header="dcsdw100", footer="edwedw100"))
+    old_groups = app.group.get_group_list()
     app.group.delete_first_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
