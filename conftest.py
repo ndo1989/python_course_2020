2265245162b53a351d56601d63d8e1ec355e19ff
dnevaykin <40721547+dnevaykin@users.noreply.github.com>
2021-01-28 00:28:53 +0300

    Задание 16, попытка 1. Реализованы опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -7,11 +7,12 @@ fixture = None
 def app(request):
     global fixture
     if fixture is None:
-        fixture = Application()
+        browser = request.config.getoption("--browser")
+        base_url = request.config.getoption("--baseUrl")
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            browser = request.config.getoption("--browser")
-            fixture = Application(browser=browser)
+            fixture = Application()
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -25,3 +26,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/")
--- fixture/application.py
+++ fixture/application.py
@@ -5,9 +5,10 @@ from pythoncourse2020.fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self, browser="firefox"):
+    def __init__(self, browser, base_url):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
+            self.wd.implicitly_wait(1)
         elif browser == "chrome":
             self.wd = webdriver.Chrome()
         elif browser == "ie":
@@ -17,7 +18,7 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-
+        self.base_url = base_url
     def is_valid(self):
         try:
             self.wd.current_url
@@ -27,7 +28,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
