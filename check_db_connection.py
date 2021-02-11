ec106d087d24255d7430d3a6943d85a16dfc7874
dnevaykin <40721547+dnevaykin@users.noreply.github.com>
2021-02-11 20:00:21 +0300

    Задание 20, попытка 2. Переделаны тесты, чтобы сравнивались данные, загруженные из БД. Реализованы отключаемые проверки пользовательского интерфейса -- сравнение данных, полученных из пользовательского интерфейса, с данными, загруженными из базы данных.

---
+++ check_db_connection.py
@@ -0,0 +1,20 @@
+import pymysql.cursors
+from pythoncourse2020.fixture.db import DbFixture
+
+db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+try:
+    contacts = db.get_contact_list()
+    for contact in contacts:
+       print(contact)
+    print(len(contacts))
+finally:
+    db.destroy()
+
+#try:
+    #groups = db.get_group_list()
+    #for group in groups:
+       #print(group)
+    #print(len(groups))
+#finally:
+    #db.destroy()
