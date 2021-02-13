557e6f7bb5401ec640cdef225884cd31f8bd34e9
dnevaykin <40721547+dnevaykin@users.noreply.github.com>
2021-02-13 14:57:53 +0300

    Задание 21, попытка 2. Доработан кейс для проверки информации о контактах на главной странице согласно замечаниям

--- test/test_comparator_contact_on_home_page_and_db.py
+++ test/test_comparator_contact_on_home_page_and_db.py
@@ -8,7 +8,7 @@ def test_comparator_contact_on_home_page_and_db(app, db):
     contacts_home_page = app.contact.get_contact_list()
     contacts_db = db.get_contact_list()
     list_contacts_from_home_page = list(
-        map(lambda y: (y.id, y.firstname, y.lastname, y.all_emails_from_home_page, y.all_phones_from_home_page), contacts_home_page))
+        map(lambda y: (y.id, y.firstname, y.lastname, y.all_emails_from_home_page, y.all_phones_from_home_page), contacts_home_page ))
     list_contacts_from_db = list(
         map(lambda y: (y.id, y.firstname, y.lastname, merge_emails_like_on_home_page(y), merge_phones_like_on_home_page(y)), contacts_db))
     assert sorted(list_contacts_from_home_page) == sorted(list_contacts_from_db)
